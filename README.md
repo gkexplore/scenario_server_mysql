9174fa0b390fb7bf422238d36d9a7a940ccd2ff0
Karthik Ganesan <gkexplore@gmail.com>
2015-03-20 15:00:38 -0700

    removing all the files

--- README.md
+++
@@ -1,3 +0,0 @@
-# Scenario_Server
-It is a web application which can be used as a web service for web application/Native App
-# scenario_server_mysql
--- test/controllers/scenarios_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ScenariosControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
