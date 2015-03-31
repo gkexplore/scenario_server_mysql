331755de25ee1be14daafc5c63ced47b0b28f244
Karthik <gkexplore@gmail.com>
2015-03-31 15:04:24 -0700

    adding import feature for new scenario server

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -1,6 +1,7 @@
 class FeaturesController < ApplicationController
 	#http_basic_authenticate_with name: "dhh", password: "secret", only: :index
 	 skip_before_filter :verify_authenticity_token
+	 include FeaturesHelper
 	def index
 		@features = Feature.all
 	end
@@ -37,6 +38,22 @@ class FeaturesController < ApplicationController
 			file_name = Time.now.to_s<<".json"
 			send_data @features.as_json.to_json, :type=>'json', :disposition => 'attachment', :filename => 'Stubs_'<<file_name
 	end
+
+	def import_json_index
+
+	end
+
+	def import_json
+		file = params[:upload]
+		begin
+			store_json(file)
+			File.delete("public/#{file['datafile'].original_filename}")
+		rescue=>e
+			puts "An error has been occurred while importing the data!!! #{e.class.name}: #{e.message}"
+		end
+		redirect_to '/'
+	end
+
 	private
 	 def feature_params
 		params.require(:feature).permit(:feature_name)
--- app/helpers/features_helper.rb
+++ app/helpers/features_helper.rb
@@ -1,2 +1,26 @@
 module FeaturesHelper
+	def store_json(file)
+		directory ="public"
+		path = File.join(directory,file['datafile'].original_filename)
+		File.open(path,"wb"){|f| f.write(file['datafile'].read)}
+		json_string = File.read("#{directory}/#{file['datafile'].original_filename}")
+		proper_json = json_string.gsub('\"', '"')
+		logs = JSON.parse(proper_json)
+		logs.each do |feature|
+			feature_model = Feature.find_or_initialize_by(:id=>feature['id'],:feature_name=>feature['feature_name'])
+			feature_model.update(:id=>feature['id'],:feature_name=>feature['feature_name'])
+			feature['flows'].each do |flow|
+				flows = feature_model.flows.find_or_initialize_by(:flow_name=>flow['flow_name'])
+				flows.update(:flow_name=>flow['flow_name'])
+				flow['scenarios'].each do |scenario|
+					scenarios = flows.scenarios.find_or_initialize_by(:scenario_name=>scenario['scenario_name'])
+					scenarios.update(:scenario_name=>scenario['scenario_name'])
+					scenario['routes'].each do |route|
+						route = scenarios.routes.find_or_initialize_by(:route_type=>route['route_type'],:path=>route['request_url'],:request_body=>route['request_body'],:fixture=>route['response'],:status=>route['status'],:host=>route['host'])
+						route.update(:route_type=>route['route_type'],:path=>route['request_url'],:request_body=>route['request_body'],:fixture=>route['response'],:status=>route['status'],:host=>route['host'])
+					end
+				end
+			end
+		end
+	end
 end
