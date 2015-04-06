d0e87027f75a291a14b30163aa440810437e27b9
Karthik Ganesan <gkexplore@gmail.com>
2015-04-05 19:32:06 -0700

    adding UI changes

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -12,7 +12,10 @@ class RoutesController < ApplicationController
 
 	end
 	def show
-
+		@feature = Feature.find(params[:feature_id])
+		@flow = @feature.flows.find(params[:flow_id])
+		@scenario = @flow.scenarios.find(params[:scenario_id])
+		@route = @scenario.routes.find(params[:id])
 	end
 	def update
 		
