8856ad6768a4e12907194ad681a1e1ae4b31358d
karthik <gkexplore@gmail.com>
2015-04-20 18:30:32 -0700

    adding changes for sorting query parameters

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -8,7 +8,9 @@ class RoutesController < ApplicationController
 			@feature = Feature.find(params[:feature_id])
 	    	@flow = @feature.flows.find(params[:flow_id])
 	    	@scenario = @flow.scenarios.find(params[:scenario_id])
-	    	@route = @scenario.routes.create(route_params)
+	    	params = route_params
+			params[:path] = sort_query_parameters(params)
+	    	@route = @scenario.routes.create(params)
 	    	render "scenarios/show"
 	    rescue=>e
 			render :text => "An error has been occurred while creating the route #{e.class.name}: #{e.message}"
@@ -43,7 +45,9 @@ class RoutesController < ApplicationController
 			@flow = @feature.flows.find(params[:flow_id])
 			@scenario = @flow.scenarios.find(params[:scenario_id])
 			@route = @scenario.routes.find(params[:id])
-			  if @route.update(route_params)
+			params = route_params
+			params[:path] = sort_query_parameters(params)
+			  if @route.update(params)
 			    render "scenarios/show"
 			  else
 			    render 'routes/edit'
@@ -68,4 +72,38 @@ class RoutesController < ApplicationController
 	 def route_params
 		params.require(:route).permit(:route_type,:path,:query,:request_body,:fixture,:status,:host)
 	 end
+	private
+	 def qs_to_hash(querystring)
+		  keyvals = querystring.split('&').inject({}) do |result, q| 
+		    k,v = q.split('=')
+		    if !v.nil?
+		       result.merge({k => v})
+		    elsif !result.key?(k)
+		      result.merge({k => true})
+		    else
+		      result
+		    end
+		  end
+		  keyvals
+	end
+	private
+	def sort_query_parameters(params)
+		path_query = params[:path]
+    	temp_url = URI.parse("http://localhost:9090"+path_query)
+    	query = temp_url.query
+    	if query==nil || query=='' || query.blank?
+    		path = temp_url.path.downcase
+    	else
+	    	puts "query ***"+query
+	    	path = temp_url.path.downcase
+			hash_string = qs_to_hash(query)
+			sorted_string =  Hash[hash_string.sort]
+			final_sorted_string = sorted_string.to_query
+			if final_sorted_string.to_s.strip.length != 0
+				final_sorted_string = "?"<<final_sorted_string
+			end
+			path+final_sorted_string
+		end
+	end
+
 end
