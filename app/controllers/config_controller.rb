d55fa2a0b0eb510d1e7f01381dd3afb414d58a2f
Karthik <gkexplore@gmail.com>
2015-04-23 14:01:20 -0700

    adding UI change for config

--- app/controllers/config_controller.rb
+++ app/controllers/config_controller.rb
@@ -1,10 +1,24 @@
 class ConfigController < ApplicationController
   def index
-  	puts "Inside index"
 		begin
 			@configs = Config.all
 		rescue=>e
 			render :text =>"An error has been occurred while retrieving all the features #{e.class.name}: #{e.message}"
 		end
 	end
+	def update
+		begin
+			@config = Config.find(params[:id])
+			  if @config.update(config_params)
+			  	@configs = Config.all
+			    render action: "index"
+			   end
+		rescue=>e
+			render :text => "An error has been occurred while updating the feature #{e.class.name}: #{e.message}"
+		end
+	end
+	private
+	 def config_params
+		params.require(:config).permit(:server_mode)
+	 end
 end
