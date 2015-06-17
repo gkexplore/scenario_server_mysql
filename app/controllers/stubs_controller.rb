203d3792487272be2b357d46b3eda86daa3efaf5
Karthik <gkexplore@gmail.com>
2015-06-17 16:47:00 -0700

    updating gem file and adding delete feature in stubs UI

--- app/controllers/stubs_controller.rb
+++ app/controllers/stubs_controller.rb
@@ -1,16 +1,28 @@
 class StubsController < ApplicationController
+  
   def index
   	
   end
+  
   def poll_log
   	@stubs = Stub.all.reverse
   end
+ 
   def clear_logs
   	begin
-  		Stub.delete_all
-  		render action: "index"
- 	rescue Exception=>e
- 		render :text => "An error has been occurred while deleting the stubs #{e.class.name}: #{e.message}"
- 	end
+  		  Stub.delete_all
+  		  render action: "index"
+ 	  rescue Exception=>e
+ 		   render :text => "An error has been occurred while deleting the stubs #{e.class.name}: #{e.message}"
+  	end
   end
+  def destroy
+    begin
+      @stub = Stub.find(params[:id])
+      @stub.destroy
+    rescue=>e
+      render :text => "An error has been occurred while deleting the stub #{e.class.name}: #{e.message}"
+    end
+  end
+
 end
