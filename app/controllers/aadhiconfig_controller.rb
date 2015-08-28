177d1486e209030bc4bbfe6d0107ff118331357e
Karthik <gkexplore@gmail.com>
2015-08-28 12:00:27 -0700

    Aadhi: refactoring add code base - Introducing flash to display alert messages

--- app/controllers/aadhiconfig_controller.rb
+++ app/controllers/aadhiconfig_controller.rb
@@ -1,5 +1,7 @@
 class AadhiconfigController < ApplicationController
-	skip_before_filter :verify_authenticity_token
+
+  skip_before_filter :verify_authenticity_token
+
   def index
 		begin
 			@configs = Aadhiconfig.all
@@ -8,22 +10,27 @@ class AadhiconfigController < ApplicationController
 				@configs = Aadhiconfig.all
 			end
 		rescue=>e
-			 alert(AadhiConstants::ALERT_ERROR, "An error has been occurred while retrieving the configs #{e.class.name}: #{e.message}", "/adhiconfig", AadhiConstants::ALERT_BUTTON)
+			flash[:error] = "An error has been occurred while retrieving the configs #{e.class.name}: #{e.message}"
 		end
 	end
+
 	def update
 		begin
 			@config = Aadhiconfig.find(params[:id])
-			  if @config.update(config_params)
-			  	@configs = Aadhiconfig.all
-	    		alert(AadhiConstants::ALERT_CONFIRMATION, "The config has been updated successfully!!!", "/aadhiconfig", AadhiConstants::ALERT_BUTTON)
-			   end
+			if @config.update(config_params)
+			   @configs = Aadhiconfig.all
+			   flash[:success] = "The config has been updated successfully!!!"
+			   render 'index'
+			end
 		rescue=>e
-			alert(AadhiConstants::ALERT_ERROR, "An error has been occurred while updating the server mode #{e.class.name}: #{e.message}", "/adhiconfig", AadhiConstants::ALERT_BUTTON)
+			flash[:error] = "An error has been occurred while updating the server mode #{e.class.name}: #{e.message}"
+			redirect_to '/adhiconfig'
 		end
 	end
+
 	private
 	 def config_params
 		params.require(:aadhiconfig).permit(:server_mode,:isProxyRequired,:url,:port,:user,:password,:bypass_proxy_domains)
 	 end
+
 end
--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -10,20 +10,20 @@ class FeaturesController < ApplicationController
 			@features = Feature.all
 			@flows = Flow.all
 		rescue=>e
-			alert(AadhiConstants::ALERT_ERROR, "An error has been occurred while retrieving all the features #{e.class.name}: #{e.message}", "/features", AadhiConstants::ALERT_BUTTON)
+			flash[:error] = "An error has been occurred while retrieving all the features #{e.class.name}: #{e.message}"
 		end
 	end
 
 	def create
 		begin
 			@feature = Feature.new(feature_params)
-	  		if @feature.save
-	  			redirect_to @feature
-	  		else
-	  			render 'new'
-	  		end
+  			if @feature.save
+  				flash[:success] = "The feature has been created successfully!!!"
+  				redirect_to @feature
+  			end
   		rescue=>e
-			alert(AadhiConstants::ALERT_ERROR, "An error has been occurred while retrieving the feature #{e.class.name}: #{e.message}", "/features", AadhiConstants::ALERT_BUTTON)
+			flash[:error] = "An error has been occurred while creating the feature!!!"
+	  		render 'new'
   		end
 	end
 	
@@ -31,7 +31,7 @@ class FeaturesController < ApplicationController
 		begin
 			@feature = Feature.new
 		rescue=>e
-			alert(AadhiConstants::ALERT_ERROR, "An error has been occurred while creating new instance for a feature #{e.class.name}: #{e.message}", "/features", AadhiConstants::ALERT_BUTTON)
+			flash[:error] = "An error has been occurred while creating new instance for a feature #{e.class.name}: #{e.message}"
 		end
 	end
 	
@@ -39,7 +39,7 @@ class FeaturesController < ApplicationController
 		begin
 			@feature = Feature.find(params[:id])
 		rescue=>e
-			alert(AadhiConstants::ALERT_ERROR, "An error has been occurred while editing the feature #{e.class.name}: #{e.message}", "/features", AadhiConstants::ALERT_BUTTON)
+			flash[:error] = "An error has been occurred while editing the feature #{e.class.name}: #{e.message}"
 		end
 	end
 	
@@ -49,14 +49,14 @@ class FeaturesController < ApplicationController
 	
 	def update
 		begin
-			@feature = Feature.find(params[:id])
-			  if @feature.update(feature_params)
-			    redirect_to @feature
-			  else
-			    render 'edit'
-			  end
+		  @feature = Feature.find(params[:id])
+		  if @feature.update(feature_params)
+		  	flash[:success] = "The feature has been updated successfully!!!"
+		    redirect_to @feature
+		  end
 		rescue=>e
-			alert(AadhiConstants::ALERT_ERROR, "An error has been occurred while updating the feature #{e.class.name}: #{e.message}", "/features", AadhiConstants::ALERT_BUTTON)
+			flash[:danger] = "An error has been occurred while updating the feature!!!"
+			render 'edit'
 		end
 	end
 	
@@ -64,9 +64,11 @@ class FeaturesController < ApplicationController
 		begin
 			@feature = Feature.find(params[:id])
 			@feature.destroy
-			alert(AadhiConstants::ALERT_CONFIRMATION, "The selected feature has been deleted successfully!!!", "/features", AadhiConstants::ALERT_BUTTON)
+			flash[:success] = "The selected feature has been deleted successfully!!!"
+			redirect_to '/features'
   		rescue=>e
-			alert(AadhiConstants::ALERT_ERROR, "An error has been occurred while deleting the feature #{e.class.name}: #{e.message}", "/features", AadhiConstants::ALERT_BUTTON)
+  			flash[:error] = "An error has been occurred while deleting the feature #{e.class.name}: #{e.message}"
+  			redirect_to '/features'
 		end
 	end
 	
@@ -75,7 +77,8 @@ class FeaturesController < ApplicationController
 			file_name = Time.now.to_s<<".xml"
 			send_data Feature.export_as_xml(params[:feature_ids]), :type=>'xml', :disposition => 'attachment', :filename => 'Stubs_'<<file_name
 		rescue=>e
-			alert(AadhiConstants::ALERT_ERROR, "An error has been occurred while exporting the report!!! #{e.class.name}: #{e.message}", "/features", AadhiConstants::ALERT_BUTTON)
+			flash[:error] = "An error has been occurred while exporting the report!!! #{e.class.name}: #{e.message}"
+			redirect_to '/features'
 		end
 	end
 
@@ -84,12 +87,12 @@ class FeaturesController < ApplicationController
 			file_name = Time.now.to_s<<".xml"
 			send_data Feature.export_all_as_xml, :type=>'xml', :disposition => 'attachment', :filename => 'Stubs_'<<file_name
 		rescue=>e
-			alert(AadhiConstants::ALERT_ERROR, "An error has been occurred while exporting the report!!! #{e.class.name}: #{e.message}", "/features", AadhiConstants::ALERT_BUTTON)
+			flash[:error] = "An error has been occurred while exporting the report!!! #{e.class.name}: #{e.message}"
+			redirect_to '/features'
 		end
 	end
 
 	def import_xml_index
-
 	end
 
 	def import_xml
@@ -98,12 +101,15 @@ class FeaturesController < ApplicationController
 			if file['datafile'].content_type=="text/xml"
 				store_json(file)
 				File.delete("public/#{file['datafile'].original_filename}")
-        		alert(AadhiConstants::ALERT_CONFIRMATION, "Your stubs have been uploaded successfully!!!", "/features", AadhiConstants::ALERT_BUTTON)
+				flash[:success] = "Your stubs have been uploaded successfully!!!"
+				redirect_to '/features'
 			else
-				alert(AadhiConstants::ALERT_ERROR, "Please upload a valid xml file!!!", "/features", AadhiConstants::ALERT_BUTTON)
+				flash.now[:warning] = "Please upload a valid xml file!!!"
+				render 'import_xml_index'
 			end
 		rescue=>e
-				alert(AadhiConstants::ALERT_ERROR, "An error has been occurred while importing the stubs!!! #{e.class.name}: #{e.message}", "/features", AadhiConstants::ALERT_BUTTON)
+				flash.now[:danger] = "An error has been occurred while importing the stubs!!!"
+				render 'import_xml_index'
 		end
 	end
 
