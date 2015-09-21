bead1446ff93dada6b15f10482768ec0b5852cce
Karthik <gkexplore@gmail.com>
2015-09-21 00:17:37 -0700

    Aadhi: adding dropdown to display 404 based on device ip and adding clear server log in header

--- app/controllers/notfound_controller.rb
+++ app/controllers/notfound_controller.rb
@@ -3,16 +3,20 @@ class NotfoundController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-  	@notfound_list = Notfound.all
+     @notfound_list = Notfound.pluck(:device_ip).uniq
   end
 
   def notfound_list
-	begin
-		@notfound_list = Notfound.all.reverse
-		render layout: false
-	rescue=>e
-		flash[:error] = "An error has been occurred while retrieving the device and scenario details"
-	end
+  	begin
+      if params[:device_ip].empty? || params[:device_ip].blank?
+      	 @notfound_list = Notfound.all.reverse 
+      else
+         @notfound_list = Notfound.where(:device_ip=>params[:device_ip]) 
+      end
+      render layout: false
+  	rescue=>e
+  		flash[:error] = "An error has been occurred while retrieving the device and scenario details"
+  	end
   end
 
   def clear_notfound_list
