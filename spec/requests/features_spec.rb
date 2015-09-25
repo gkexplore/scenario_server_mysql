40bd82852b9546d05a1a4e88bd7e0b1b200e8ff6
Karthik <gkexplore@gmail.com>
2015-09-25 14:39:31 -0700

    Aadhi: Adding feature spec

--- spec/requests/features_spec.rb
+++ spec/requests/features_spec.rb
@@ -2,15 +2,21 @@ require 'rails_helper'
 
 describe "feature", :type => :feature do
   describe "Manage Feature" do
-    it "Adds a new contact and displays the results" do
+    it "Adds a new contact and displays the results", :js => true do
       visit '/features'
-      expect{
         click_link 'New Feature'
         fill_in 'feature[feature_name]', :with => 'feature1'
         click_button "Create Feature"
-      }.to change(Feature,:count).by(1)
       expect(page).to have_content "The feature has been created successfully!!!"
       expect(page).to have_content "feature1"
+      expect(page).to have_submit_button "Create Flow"
+      expect(page).to have_content "Flow Name"
     end
+  end
+end
+
+RSpec::Matchers.define :have_submit_button do |value|
+  match do |actual|
+    expect(actual).to have_selector("input[type=submit][value='#{value}']")
   end
 end
