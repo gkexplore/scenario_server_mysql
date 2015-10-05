949b5af644cbb5c0b99fe6232f8c2293c7e1d030
Karthik <gkexplore@gmail.com>
2015-10-05 11:46:56 -0700

    Aadhi: Adding validation to report export feature in Aadhi

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -5,8 +5,17 @@ class ReportController < ApplicationController
   end
 
   def export
-  	  file_name = Time.now.to_s<<".xml"
-	  send_data DeviceReport.export_as_xml(params[:device_ip]), :type=>'xml', :disposition => 'attachment', :filename => 'Reports_'<<file_name	
+  	  if params[:device_ip].nil? || params[:device_ip].empty? || params[:device_ip].blank?
+  	  	 render :json => { :status => '404', :message => 'No Record Found'}, :status => 404
+  	  else
+  	  	 @device = DeviceReport.find_by(:device_ip=>params[:device_ip])
+  	  	    if @device.blank?
+  	  	    	render :json => { :status => '404', :message => 'No Record Found'}, :status => 404
+  	  	    else
+  	 			 file_name = Time.now.to_s<<".xml"
+	  			 send_data DeviceReport.export_as_xml(params[:device_ip]), :type=>'xml', :disposition => 'attachment', :filename => 'Reports_'<<file_name	
+ 	  		end
+ 	  end
   end
 
 end
