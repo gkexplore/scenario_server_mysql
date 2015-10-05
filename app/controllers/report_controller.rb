9115162b044f2d5810897ce1cf29a86b112f69e6
Karthik <gkexplore@gmail.com>
2015-10-04 20:29:00 -0700

    Aadhi: Adding export report feature in Aadhi

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -4,4 +4,9 @@ class ReportController < ApplicationController
   	  @device_list = DeviceReport.all
   end
 
+  def export
+  	  file_name = Time.now.to_s<<".xml"
+	  send_data DeviceReport.export_as_xml(params[:device_ip]), :type=>'xml', :disposition => 'attachment', :filename => 'Reports_'<<file_name	
+  end
+
 end
--- app/models/device_report.rb
+++ app/models/device_report.rb
@@ -1,3 +1,7 @@
 class DeviceReport < ActiveRecord::Base
 	has_many:device_scenarios, dependent: :delete_all
+	def self.export_as_xml(device_ip)
+      	  @reports = DeviceReport.where(:device_ip=>device_ip).includes({:device_scenarios =>:scenario_routes}).joins({:device_scenarios =>:scenario_routes})
+      	  @reports.to_xml(:include => {:device_scenarios => {:include => :scenario_routes}})
+    end
 end
